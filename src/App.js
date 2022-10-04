1c998e656edb9d6f6d2a51359fb7f1b719626d39
MishaKravchenko <mishakravchenko.1998@gmail.com>
2022-10-04 12:10:53 +0300

    notebook map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Notebook from "./components/Notebook/Notebook";
+import Notebooks from "./components/Notebooks/Notebooks";
 
 function App() {
     return (
-        <div className={"item"}>
-            <Notebook id={"1"} brand={"Apple"} model={"Pro"} cost={"100$"} url={"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp-spacegray-select-202206?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1653493200207"} alt={"Apple pro"}/>
-            <Notebook id={"2"} brand={"HP"} model={"15s"} cost={"1000$"} url={"https://img.moyo.ua/img/gallery/5141/91/1363303_middle.jpg?1650986128"} alt={"HP 15s"}/>
-            <Notebook id={"3"} brand={"Dell"} model={"Inspiron5584"} cost={"10$"} url={"https://www.startech.com.bd/image/cache/catalog/laptop/dell/5584/inspiron-5584-i3-4-500x500.jpg"} alt={"Dell Inspiron5584"}/>
+        <div>
+            <Notebooks/>
         </div>
     );
 }
--- src/components/Notebook/Notebook.js
+++ src/components/Notebook/Notebook.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Notebook.css';
 
-const Notebook = (props) => {
-    let { id, brand, model, cost, url, alt } = props;
+const Notebook = ({notebook}) => {
+    let {id, brand, model, cost, url, alt} = notebook
     return (
-        <div>
+        <div className={"item"}>
             <h1>{id}. Brand: {brand}</h1>
             <p>Model: {model} - {cost}</p>
             <img src={url} alt={alt}/>

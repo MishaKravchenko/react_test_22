88b7a7dc5dcd0bfcff0f9443a4f498a40a48082a
MishaKravchenko <mishakravchenko.1998@gmail.com>
2022-10-05 21:04:22 +0300

    lesson2_test2 (onClick, useState, counter)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import './App.css';
-import Test from "./components/Test";
+// import './App.css';
+// import {useState} from "react";
+//
+// function App() {
+//     const [greeting, setGreeting] = useState('Hello');
+//
+//     // let greeting = "Hello";
+//     const adder = () => {
+//         // greeting+= "Amigos";
+//         setGreeting(greeting+'!')
+//         console.log(greeting);
+//     }
+//
+//     return (
+//         <div>
+//             <button onClick={adder}>Click</button>
+//             {greeting}
+//         </div>
+//     );
+// }
+//
+// export default App;
+import React, {useState} from 'react';
 
-function App() {
-   const user = { name: "Vasia", age: 12}
+const App = () => {
+    const [count, setCount] = useState(0);
+    const counter = () => {
+        setCount(count + 1);
+        console.log(count)
+    }
     return (
         <div>
-        <Test>
-            ChildrenText,
-            {user};
-        </Test>
+            <button onClick={counter}>Click</button>
+            I clicked {count} times.
         </div>
     );
-}
+};
 
 export default App;

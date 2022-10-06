a9c2e05a6073a036f7762c2cbe2e1f5c4d85be40
MishaKravchenko <mishakravchenko.1998@gmail.com>
2022-10-06 19:36:10 +0300

    lesson2_test6 (select, form, option)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import Users from "./components/Users";
+import React, {useEffect, useState} from 'react';
+import User from "./components/User";
 
 const App = () => {
+    const [users, setUsers] = useState([]);
+    const [userId, setUserId] = useState({});
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(value => setUsers(value))
+    }, []);
+
+    const submit = (e) => {
+        e.preventDefault()
+            fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+                .then(value => value.json())
+                .then(value => setUser(value))
+    }
+
+    const selectHandler = (e) => {
+        const id = e.target.value
+        setUserId(id)
+    }
+
     return (
         <div>
-            <Users/>
+            <form onSubmit={submit}>
+                <select onChange={selectHandler}>
+                    {users.map(value => <option key={value.id} value={value.id}>{value.id}) {value.name} - {value.username}</option>)}
+                </select>
+                <button>Submit</button>
+            </form>
+            {user && <User user={user}/> }
         </div>
     );
 };
--- src/configs/urls.js
+++
@@ -1,7 +0,0 @@
-const baseURL = 'https://jsonplaceholder.typicode.com';
-
-export const urls = {
-    users: `${baseURL}/users`,
-    posts: `${baseURL}/posts`,
-    comments: `${baseURL}/comments`
-}

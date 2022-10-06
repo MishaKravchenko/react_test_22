f5f3a2a21ea99b9ae068ae9cca592cf877a6fde1
MishaKravchenko <mishakravchenko.1998@gmail.com>
2022-10-06 17:25:53 +0300

    lesson2_test5 (form, useState, onSubmit, onChange)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+const App = () => {
+
+    const [form, setForm] = useState({name: '', age: 0});
+
+    const submit = (e) => {
+        e.preventDefault();
+        console.log(form);
+    }
+
+    const formHandle = (e) => {
+        setForm({...form, [e.target.name]: e.target.value})
+    }
+
     return (
         <div>
-
+            <form onSubmit={submit}>
+                <div>
+                    <label>Name: <input type="text" name={"name"} onChange={formHandle}/></label>
+                    <label>Age: <input type="number" name={"age"} onChange={formHandle}/></label>
+                    <button>Submit</button>
+                </div>
+            </form>
         </div>
     );
-}
+};
 
 export default App;

c0c76565a5ddc37e3da1fdce8b57cac43c47c924
MishaKravchenko <mishakravchenko.1998@gmail.com>
2022-10-12 11:52:38 +0300

    useSearchParams, queryParams, filter

--- src/pages/PostsPage/PostsPage.js
+++ src/pages/PostsPage/PostsPage.js
@@ -1,17 +1,35 @@
 import React, {useEffect, useState} from 'react';
-import {Outlet} from "react-router-dom";
+import {Outlet, useSearchParams} from "react-router-dom";
 
 import {postService} from "../../services/post.service";
 import Post from "../../components/Post/Post";
 
 const PostsPage = () => {
     const [posts, setPosts] = useState([]);
+    let [query, setQuery] = useSearchParams();
 
     useEffect(() => {
-        postService.getAll().then(value => setPosts(value))
-    }, []);
+        postService.getAll().then(value => {
+            const title = query.get('title');
+            let filter = [...value]
+            if (title) {
+                filter = value.filter(post => post.title.includes(title))
+            }
+            setPosts(filter)
+        })
+    }, [query]);
+
+    const submit = (e) => {
+        e.preventDefault();
+        setQuery({title: e.target.search.value})
+    }
+
     return (
         <div>
+            <form onSubmit={submit}>
+                <input type="search" name={"search"}/>
+                <button>Search</button>
+            </form>
             <h1>Posts</h1>
             {posts.map(value => <Post key={value.id} post={value}/>)}
             <div><Outlet/></div>

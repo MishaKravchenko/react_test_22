acc88e09e3d0d4c320914a7725b21c65177d0d3a
MishaKravchenko <mishakravchenko.1998@gmail.com>
2022-10-19 15:31:15 +0300

    add component Counter

--- src/App.js
+++ src/App.js
@@ -1,74 +1,37 @@
-import {useReducer} from "react";
-
 import './App.css';
+import {useReducer} from "react";
+import Counter from "./components/Counter/Counter";
 
 const reducer = (state, action) => {
-    switch (action.type) {
-        case 'inc1':
-            return {...state, count1: state.count1 + 1}
-    }
-    switch (action.type) {
-        case 'dec1':
-            return {...state, count1: state.count1 - 1}
-    }
-    switch (action.type) {
-        case 'reset1':
-            return {...state, count1:0}
-    }
-    switch (action.type) {
-        case 'inc2':
-            return {...state, count2: state.count2 + 1}
-    }
-    switch (action.type) {
-        case 'dec2':
-            return {...state, count2: state.count2 - 1}
-    }
-    switch (action.type) {
-        case 'reset2':
-            return {...state, count2: 0}
-    }
-    switch (action.type) {
-        case 'inc3':
-            return {...state, count3: state.count3 + 1}
-    }
-    switch (action.type) {
-        case 'dec3':
-            return {...state, count3: state.count3 - 1}
-    }
-    switch (action.type) {
-        case 'reset3':
-            return {...state, count3: 0}
+    const {type, payload} = action;
+
+    switch (type) {
+        case'count1':
+            return {...state, count1: state.count1 + payload}
+        case'count2':
+            return {...state, count2: state.count2 + payload}
+        case'count3':
+            return {...state, count3: state.count3 + payload}
+
+        case'reset':
+            return {...state, ['count' + payload]: 0}
+
+        default:
+            console.error('Unknown type');
+            return state
     }
 }
 
+
 function App() {
 
-    const [state, dispatch] = useReducer(reducer, {
-        count1: 0,
-        count2: 0,
-        count3: 0,
-    });
+    const [state, dispatch] = useReducer(reducer, {count1: 0, count2: 0, count3: 0});
 
     return (
-        <div>
-            <div className={"counter1"}>
-                <div>{state.count1}</div>
-                <button onClick={() => dispatch({type: 'inc1'})}>INC</button>
-                <button onClick={() => dispatch({type: 'dec1'})}>DEC</button>
-                <button onClick={() => dispatch({type: 'reset1'})}>RESET</button>
-            </div>
-            <div className={"counter2"}>
-                <div>{state.count2}</div>
-                <button onClick={() => dispatch({type: 'inc2'})}>INC</button>
-                <button onClick={() => dispatch({type: 'dec2'})}>DEC</button>
-                <button onClick={() => dispatch({type: 'reset2'})}>RESET</button>
-            </div>
-            <div className={"counter3"}>
-                <div>{state.count3}</div>
-                <button onClick={() => dispatch({type: 'inc3'})}>INC</button>
-                <button onClick={() => dispatch({type: 'dec3'})}>DEC</button>
-                <button onClick={() => dispatch({type: 'reset3'})}>RESET</button>
-            </div>
+        <div className={'wrapper'}>
+            <Counter state={state} dispatch={dispatch} counterNumber={1}/>
+            <Counter state={state} dispatch={dispatch} counterNumber={2}/>
+            <Counter state={state} dispatch={dispatch} counterNumber={3}/>
         </div>
     );
 }
---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Counter = ({state, dispatch, counterNumber}) => {
+    return (
+        <div>
+            <div> Count : {state['count' + counterNumber]}</div>
+
+            <button onClick={() => dispatch({type: 'count' + counterNumber, payload: 1})}>Inc</button>
+            <button onClick={() => dispatch({type: 'count' + counterNumber, payload: -1})}>Dec</button>
+            <button onClick={() => dispatch({type: 'reset', payload: counterNumber})}>Reset</button>
+        </div>
+    );
+};
+
+export default Counter;

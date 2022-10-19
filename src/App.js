c4fabf5cc1146fb7c0bc2274dd7ac3c61f7dc533
MishaKravchenko <mishakravchenko.1998@gmail.com>
2022-10-19 12:12:59 +0300

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import {useReducer} from "react";
+
 import './App.css';
 
+const reducer = (state, action) => {
+    state = {...state, count2: state.count2 + 1}
+    switch (action.type) {
+        case 'inc':
+            return {...state, count1: state.count1 + 1}
+        case 'dec':
+            return {...state, count1: state.count1 - 1}
+        case 'reset':
+            return {...state, count1: action.payload}
+        default:
+            throw new Error("MyError")
+    }
+    return state
+}
+
 function App() {
+    const [state, dispatch] = useReducer(reducer, {count1: 0, count2: 0});
+
     return (
         <div>
-
+            <div>{state.count1}</div>
+            <div>{state.count2}</div>
+            <button onClick={() => dispatch({type: 'inc'})}>INC</button>
+            <button onClick={() => dispatch({type: 'dec'})}>DIC</button>
+            <button onClick={() => dispatch({type: 'reset', payload: 8})}>RESET</button>
         </div>
     );
 }
